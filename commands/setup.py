f994a8176ea579c9a726f92e744376f10e92b4e8
Parsa Azimi <parsa.azm@gmail.com>
2022-12-07 20:18:29 -0800

    :card_file_box: Add database initial stuff

---
+++ commands/setup.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+
+def setup(name):
+    connection = sqlite3.connect(f'{name}.db')
+    cursor = connection.cursor()
+    cursor.execute("CREATE TABLE users (username TEXT, password TEXT)")
+    cursor.execute("CREATE TABLE passwords (user TEXT, source TEXT, username TEXT, password TEXT)")
+
+
+if __name__ == "__main__":
+    setup('password_manager')
