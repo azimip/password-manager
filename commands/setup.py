d254436c6acf02a59139a416cd7234d0407aee79
Parsa Azimi <parsa.azm@gmail.com>
2022-12-08 19:39:19 -0800

    :bug: Add old_passwords column to users table

--- commands/setup.py
+++ commands/setup.py
@@ -4,7 +4,7 @@ import sqlite3
 def setup(name):
     connection = sqlite3.connect(f'../{name}.db')
     cursor = connection.cursor()
-    cursor.execute("CREATE TABLE users (username TEXT PRIMARY KEY, password TEXT)")
+    cursor.execute("CREATE TABLE users (username TEXT PRIMARY KEY, password TEXT, old_passwords TEXT)")
     cursor.execute("CREATE TABLE passwords (user TEXT, source TEXT, username TEXT, password TEXT)")
 
 
